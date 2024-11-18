ebf161613d27cbe980c499725f059941353f38d6
AxelGrille1 <118001920+AxelGrille1@users.noreply.github.com>
2024-11-18 16:10:53 +0100

    API v1 + test

---
+++ src/test/java/com/human_ressources/api/EmployeeControllerTest.java
@@ -0,0 +1,34 @@
+package com.human_ressources.api;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.human_ressources.api.controller.EmployeeController;
+import com.human_ressources.api.service.EmployeeService;
+
+@SpringBootTest
+@AutoConfigureMockMvc class EmployeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetEmployees() throws Exception {
+        mockMvc.perform(get("/employees"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].firstName", is("Laurent")));
+
+    }
+
+}
